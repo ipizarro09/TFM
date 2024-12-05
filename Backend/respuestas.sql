c4fb6f379260e71b1650073cf2c8e491aae8b592
ipizarro09 <55761030+ipizarro09@users.noreply.github.com>
2024-12-05 17:54:00 +0100

    Add files via upload

---
+++ Backend/respuestas.sql
@@ -0,0 +1,25 @@
+﻿-- Table: respuestas
+
+-- DROP TABLE respuestas;
+
+CREATE TABLE respuestas
+(
+  id serial NOT NULL,
+  n_dimensiones character varying(50),
+  tipo_datos character varying(100),
+  ordenadas character varying(50) DEFAULT 'Not applicable'::character varying,
+  n_grupos_alto character varying(100) DEFAULT 'Not applicable'::character varying,
+  relacion character varying(100) DEFAULT 'Not applicable'::character varying,
+  obs_grupo character varying(50) DEFAULT 'Not applicable'::character varying,
+  proposito character varying(100),
+  dataset_size character varying(50),
+  contexto character varying(100),
+  CONSTRAINT respuestas_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE respuestas
+  OWNER TO postgres;
+GRANT ALL ON TABLE respuestas TO postgres;
+GRANT ALL ON TABLE respuestas TO ipizarro;
---
+++ Backend/vizquest.sql
@@ -0,0 +1,15 @@
+﻿-- Database: vizquest
+
+-- DROP DATABASE vizquest;
+
+CREATE DATABASE vizquest
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Spanish_Spain.1252'
+       LC_CTYPE = 'Spanish_Spain.1252'
+       CONNECTION LIMIT = -1;
+GRANT CONNECT, TEMPORARY ON DATABASE vizquest TO public;
+GRANT ALL ON DATABASE vizquest TO postgres;
+GRANT ALL ON DATABASE vizquest TO ipizarro;
+
